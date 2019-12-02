af0c66065640a66c553a13c02f209459006773bd
Aligulac <46535772+Aligulac@users.noreply.github.com>
2019-12-02 15:29:38 -0800

    Enemy Move

---
+++ ElemetnTower/Assets/Element_TD/Script/EnemyMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    private Transform target;
+    private int wavepointIndex = 0;
+    public float Speed = 10f;
+
+    void Start()
+    {
+        target = WayPoint.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * Speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= WayPoint.points.Length - 1)
+        {
+            EndPath();
+            return;
+        }
+
+        wavepointIndex++;
+        target = WayPoint.points[wavepointIndex];
+    }
+
+    void EndPath()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ ElemetnTower/Assets/Element_TD/Script/WayPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+
+}
