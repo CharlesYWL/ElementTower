2c22e7cab34bb9e5fc43954bb4963fec2758b649
zhongquan chen <czqchen@ucdavis.edu>
2019-12-07 00:33:15 -0800

    added bgm

---
+++ ElemetnTower/Assets/Element_TD/Script/Audio/BattleBGM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleBGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("battlebgm");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
