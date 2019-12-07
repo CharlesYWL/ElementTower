bb13bc7a715aa356d939d9a1b6cbe69173013c46
CharlesYWL <xlyin@ucdavis.edu>
2019-12-06 20:52:40 -0800

    Figuring out add card to hand

--- ElemetnTower/Assets/Element_TD/Script/Cards.cs
+++ ElemetnTower/Assets/Element_TD/Script/Cards.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// This class is to main cards in hand
+/// </summary>
 public class Cards : MonoBehaviour
 {
     BuildManager buildManager;
     // Start is called before the first frame update
+    public GameObject TestTarget;
     void Start()
     {
         buildManager = BuildManager.instance;
@@ -15,5 +20,14 @@ public class Cards : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // This is for test use
+    public void addPrefeb(GameObject prefeb)
+    {
+        Debug.Log("Add prefeb");
+        GameObject go;
+        go = Instantiate(prefeb, transform.position, Quaternion.identity) as GameObject;
+        go.transform.SetParent(transform);
     }
 }
