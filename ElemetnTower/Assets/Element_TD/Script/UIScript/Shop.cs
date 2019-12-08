452eb408e4bf681530de9bd2ad2d9095608d41a6
zhongquan chen <czqchen@ucdavis.edu>
2019-12-08 01:02:10 -0800

    added shop and refresh

--- ElemetnTower/Assets/Element_TD/Script/UIScript/Shop.cs
+++ ElemetnTower/Assets/Element_TD/Script/UIScript/Shop.cs
@@ -9,11 +9,15 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     BuildManager buildManager;
+    List<GameObject> CardArray;
+    //private GameObject[] CardArray;
     private int MAXCAP = 5;
     private int childCount = 0;
     // Start is called before the first frame update
     void Start()
     {
+        //CardArray = new GameObject[MAXCAP];
+        CardArray = new List<GameObject>();
         buildManager = BuildManager.instance;
     }
 
@@ -26,10 +30,10 @@ public class Shop : MonoBehaviour
     // This is for test use
     public void addPrefeb(GameObject prefeb)
     {
-        childCount++;
         GameObject go;
         go = Instantiate(prefeb, transform.position, Quaternion.identity) as GameObject;
         go.transform.SetParent(transform);
+        childCount++;
     }
     public int getCardsNumber()
     {
@@ -45,6 +49,26 @@ public class Shop : MonoBehaviour
             Destroy(this.transform.GetChild(i).gameObject);
         }
         this.childCount = 0;
+    
+    }
+
+    public void openShop()
+    {
+        foreach (GameObject child in CardArray)
+        {
+            child.SetActive(true);
+        }
+        CardArray.Clear();
+    }
+
+    public void closeShop()
+    {
+        int childs = this.transform.childCount;
+        for (int i = childs - 1; i >= 0; i--)
+        {
+            CardArray.Add(this.transform.GetChild(i).gameObject);
+            this.transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
 
     public bool isFullCap() 
