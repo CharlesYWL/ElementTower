8b12a3d63dcc1c700751e3b7d429b22e98448300
zhongquan chen <czqchen@ucdavis.edu>
2019-12-08 22:12:17 -0800

    changed main menu scene

--- ElemetnTower/Assets/Element_TD/Script/UIScript/MainMenuBtn.cs
+++ ElemetnTower/Assets/Element_TD/Script/UIScript/MainMenuBtn.cs
@@ -2,11 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuBtn : MonoBehaviour
 {
+    private bool clicked = false;
     public void StartGame()
     {
+        AudioManager.instance.Play("clickbtn", SoundType.GAME);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void StartOption()
+    {
+        AudioManager.instance.Play("clickbtn", SoundType.GAME);
+    }
+
+    public void BackOption()
+    {
+        AudioManager.instance.Play("clickbtn", SoundType.GAME);
+    }
+
+    private void swtichToggle()
+    {
+        if (clicked)
+        {
+            clicked = false;
+        }
+        else
+        {
+            clicked = true;
+        }
+    }
+
+    public void setGameVolume( float sliderValue)
+    {
+        AudioManager.instance.setGameVolume(sliderValue);
+    }
+
+    public void setMusicVolume(float sliderValue)
+    {
+        AudioManager.instance.setMusicVolume(sliderValue);
+    }
+
 }
