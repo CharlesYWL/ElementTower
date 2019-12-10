eae7d71c877213c4aaca4b5547185e9704b4ff20
CharlesYWL <xlyin@ucdavis.edu>
2019-12-09 21:12:29 -0800

    Upgrade complete

---
+++ ElemetnTower/Assets/Element_TD/Script/TowerLabel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TowerLabel : MonoBehaviour
+{
+    public enum ElementType { FireTower, GlacierTower, WindTower, OceanTower, DesertTower, ThunderTower, MountainTower, LightTower, ShadowTower, CyrstalTower, PoisonTower }
+
+    // Start is called before the first frame update
+    public ElementType elementType;
+    public int Level;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static bool Compare(GameObject tower1,GameObject tower2)
+    {
+        TowerLabel t1 = tower1.GetComponent<TowerLabel>();
+        TowerLabel t2 = tower2.GetComponent<TowerLabel>();
+        return (t1.elementType == t2.elementType && t1.Level == t2.Level);
+    }
+}
