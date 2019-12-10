6c17ace7c5e0c7e3407fbaa297c8b915a25bcdb9
CharlesYWL <xlyin@ucdavis.edu>
2019-12-10 07:10:30 -0800

    Add heart group

---
+++ ElemetnTower/Assets/Element_TD/Script/UIScript/HeartGroup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartGroup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    BuildManager bm;
+    void Start()
+    {
+        bm = BuildManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
