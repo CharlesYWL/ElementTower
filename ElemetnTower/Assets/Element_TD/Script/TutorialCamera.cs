aba7722794572fe949166aa03a95d25b9a3032fd
Aligulac <yfqyang@ucdavis.edu>
2019-12-11 12:35:49 -0800

    Gitignore

---
+++ ElemetnTower/Assets/Element_TD/Script/TutorialCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCamera : MonoBehaviour
+{
+    [SerializeField]
+    GameObject MainCamera = null;
+    [SerializeField]
+    GameObject CameraTwo = null;
+
+    AudioListener MainCameraAudio;
+    AudioListener CameraTwoAudio;
+
+    GameObject Fade;
+
+    private void Start()
+    {
+        MainCameraAudio = MainCamera.GetComponent<AudioListener>();
+        CameraTwoAudio = CameraTwo.GetComponent<AudioListener>();
+
+        MainCamera.SetActive(false);
+        MainCameraAudio.enabled = false;
+
+        Fade = GameObject.FindGameObjectWithTag("Fade");
+    }
+
+    private void SwitchCamera()
+    {
+        CameraTwo.SetActive(false);
+        CameraTwoAudio.enabled = false;
+
+        MainCamera.SetActive(true);
+        MainCameraAudio.enabled = true;
+
+        Destroy(Fade);
+    }
+
+}
